7c89fc53cd3de49aa0e2b5851def6be92af9eac5
wsw0922 <woojoo0922@gmail.com>
2026-02-07 21:23:40 +0900

    fix: get friends runtime type error

--- lib/presentation/friends/friends_screen.dart
+++ lib/presentation/friends/friends_screen.dart
@@ -1,9 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:relog/core/presentation/styles/color_styles.dart';
 import 'package:relog/core/presentation/styles/text_styles.dart';
 import 'package:relog/core/presentation/widgets/app_bar/default_app_bar.dart';
+import 'package:relog/core/presentation/widgets/dialog/custom_dialog.dart';
 import 'package:relog/core/presentation/widgets/inputs/search_text_field.dart';
 import 'package:relog/domain/friends/model/friend.dart';
 import 'package:relog/presentation/friends/providers/friends_view_providers.dart';
@@ -25,22 +27,59 @@ class FriendsScreen extends HookConsumerWidget {
     final vm = ref.read(friendsViewModelProvider.notifier);
 
     useEffect(() {
-      Future.microtask(() async {
-        await vm.loadFriends();
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        vm.loadFriends();
       });
       return null;
-    }, []);
+    }, const []);
 
     final searchController = useTextEditingController();
     useListenable(searchController);
 
-    final searchQuery = searchController.text.trim();
+    final filteredFriends = useMemoized(() {
+      final list = state.friends;
+      final q = searchController.text.trim();
 
-    final List<Friend>? filteredFriends = searchQuery.isEmpty
-      ? state.friends
-      : state.friends?.where((friend) {
-          return friend.name.contains(searchQuery);
-        }).toList();
+      if (q.isEmpty) return list;
+
+      return list.where((f) => f.name.contains(q)).toList();
+    }, [state.friends, searchController.text]);
+
+    // 오류
+    useEffect(() {
+      if (state.errorMessage != null) {
+        WidgetsBinding.instance.addPostFrameCallback((_) {
+          showCupertinoDialog(
+            context: context,
+            barrierDismissible: true, // 바깥 터치 시 다이얼로그 닫힘
+            builder: (_) => CustomDialog(
+              title: '친구 오류',
+              content: state.errorMessage!,
+              actions: [
+                CustomDialogAction(
+                  text: '확인',
+                  style: DialogActionStyle.normal,
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          );
+        });
+      }
+      return null;
+    }, [state.errorMessage]);
+
+    // 로딩 상태 표시
+    if (state.isLoading) {
+      return Scaffold(
+        backgroundColor: ColorStyles.black22,
+        body: SafeArea(
+          child: Center(
+            child: CircularProgressIndicator(color: ColorStyles.grayD3,),
+          ),
+        ),
+      );
+    }
 
     return Scaffold(
       backgroundColor: ColorStyles.black22,
@@ -71,7 +110,7 @@ class FriendsScreen extends HookConsumerWidget {
                 SearchTextField(controller: searchController,),
 
                 Expanded(
-                  child: state.friends == null || state.friends!.isEmpty
+                  child: state.friends.isEmpty
                   ? Center(
                       child: Text(
                         '등록된 친구가 없어요',
@@ -83,7 +122,7 @@ class FriendsScreen extends HookConsumerWidget {
                   : ListView.builder(
                       keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
                       padding: EdgeInsets.only(top: 16),
-                      itemCount: filteredFriends!.length,
+                      itemCount: filteredFriends.length,
                       itemBuilder: (context, index) {
                         final friend = filteredFriends[index];
                         return Column(
